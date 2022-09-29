5adbc1c5ef3fdd17f06a416311326570ccae943f
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2022-09-29 10:41:57 +0600

    question added

--- src/Components/Fitness/Fitness.js
+++ src/Components/Fitness/Fitness.js
@@ -33,7 +33,16 @@ const Fitness = () => {
             <div className="cart-container">
                 <Cart cart={cart}></Cart>
             </div>
-
+            <div className='question'>
+                <h4>question 1: how does react works ?</h4>
+                <h4>Ans: ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code</h4>
+                <br />
+                <h4>question 2: what's the difference between props and state in react?</h4>
+                <h4>Ans: Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components. The this. setState property is used to update the state values in the component.</h4>
+                <br />
+                <h4>question 3: What else does it take to load useEffect data?</h4>
+                <h4>Ans: If we're using hooks in React function components, we can load data from an API when the component first loads by using the useEffect hook ...</h4>
+            </div>
         </div>
     );
 };
