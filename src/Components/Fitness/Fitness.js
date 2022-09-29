bf635ba7a1636146a5fc9ff8265960e43ae7ccd7
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2022-09-29 08:34:54 +0600

    cart added

--- src/Components/Fitness/Fitness.js
+++ src/Components/Fitness/Fitness.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Fitness.css'
 const Fitness = () => {
     const [products, setProduct] = useState([])
-    const [cart, setCart] = useState([])
+    const [cart, setCart] = useState([0])
 
     useEffect(() => {
         fetch('data.json')
@@ -29,37 +30,7 @@ const Fitness = () => {
                 }
             </div>
             <div className="cart-container">
-                <div className='menu'>
-                    <h1>Tanvir Ahmed Rifat</h1>
-                    <p>Web-Developer</p>
-                    <div className='menu-info'>
-                        <h5>Weight: 57</h5>
-                        <h5>Height: 5.4</h5>
-                        <h5>Age: 20</h5>
-                    </div>
-                </div>
-                <div>
-                    <h2>Add A Break</h2>
-                    <div className='btn-timer'>
-                        <button>10s</button>
-                        <button>20s</button>
-                        <button>30s</button>
-                        <button>40s</button>
-                    </div>
-                </div>
-                <div>
-                    <h2>Exercise Details</h2>
-
-                    <h5 className='menu-timer'>Exercise time</h5>
-                    <h5 className='menu-timer2'>Break time</h5>
-
-                </div>
-                <p>time:{cart.length}</p>
-                <div >
-                    <button className='button'>
-                        <p>Complete</p>
-                    </button>
-                </div>
+                <Cart></Cart>
             </div>
 
         </div>
